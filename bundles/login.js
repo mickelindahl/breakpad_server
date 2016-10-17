2969f4c20dc1dccfb658c0b35474f063c8f0e0ee
Mikael Lindahl <mikael.lindahl@greencargo.com>
2016-10-17 10:58:37 +0200

    clean up

--- bundles/login.js
+++ bundles/login.js
@@ -7,7 +7,7 @@ var $_=require('jquery');
 
 // Login
 function login() {
-    var  geturl=$_.ajax( {
+    $_.ajax( {
         type: 'POST',
         url: '/login',
         data: {
@@ -15,8 +15,6 @@ function login() {
             password: $_( "#password" ).val(),
         },
         success: function ( response ) {
-
-            alert("done!"+ geturl.getAllResponseHeaders());
 
             var redirect = document.URL.split( '=' );
             var url = redirect.length > 1 ? redirect[1] : location.origin + "/";
